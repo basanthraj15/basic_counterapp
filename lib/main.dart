585d272b0eaa350364b11955886e2a0dfa6214ed
basanthraj15 <basanthraj.2002@gmail.com>
2024-03-24 19:48:41 +0530

    Counter completed

--- lib/main.dart
+++ lib/main.dart
@@ -38,7 +38,7 @@ class MyHomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        backgroundColor: Colors.amber[600],
         title: Text(title),
       ),
       body: Center(
